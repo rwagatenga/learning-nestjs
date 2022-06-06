7c2c25b1cad8a1172f46b191a13d532ed12c67dd
Fred_Rwagatenga <fred.rwagatenga@gitstart.dev>
2022-06-06 10:23:15 +0200

    LN-008 testing item service in controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -9,18 +9,19 @@ import {
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { ItemsService } from './items.service';
-import { Item } from './interfaces/item'
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemService: ItemsService) {}
   @Get()
-  findAll(): string {
-    return 'Get All Items';
+  findAll(): Item[] {
+    return this.itemService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param() param): CreateItemDto {
-    return param;
+  findOne(@Param() param): Item {
+    return this.itemService.findOne(param.id);
   }
 
   @Post()
